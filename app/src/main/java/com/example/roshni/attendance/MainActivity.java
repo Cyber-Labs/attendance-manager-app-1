2e5fd5c10c5b97eec272452d604c8f20187420ce
Aman563 <amanayush563@gmail.com>
2018-12-21 01:24:11 +0530

    drawer

--- app/src/main/java/com/example/roshni/attendance/MainActivity.java
+++ app/src/main/java/com/example/roshni/attendance/MainActivity.java
@@ -4,6 +4,7 @@ import android.database.Cursor;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -21,6 +22,8 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         myDb=new DataBaseHelper(this);
         b1=(Button)findViewById(R.id.add);
         b2=(Button)findViewById(R.id.view);
